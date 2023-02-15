3a6a105357bb0b3c6d33e0045703f37326f5b684
Tree <gmldbs1109@naver.com>
2023-02-15 16:38:52 +0900

    신고결과 받기 통과

--- src/main/java/com/codingTest/programmers/level1/ReportRemain.java
+++ src/main/java/com/codingTest/programmers/level1/ReportRemain.java
@@ -6,6 +6,57 @@ import java.util.stream.*;
 public class ReportRemain {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
+
+        //내가 신고한 사람(set에 있는 사람이 k보다 높은 신고 횟수라면 내가 report받아야함)
+        Map<String, Set<String>> reportedMap = new HashMap<>();
+        //나를 신고한 사람 (set의 size가 내가 신고당한 횟수), 모든 아이디가 들어있음
+        Map<String, Set<String>> idMap = new HashMap<>();
+        for(int i = 0; i < id_list.length; i++) {
+            idMap.put(id_list[i], new HashSet<String>());
+        }
+
+        for(int i = 0; i < report.length; i++) {
+            String reporter = report[i].split(" ")[0];
+            String reportee = report[i].split(" ")[1];
+            if(reportedMap.get(reporter) != null) {
+                Set<String> set = reportedMap.get(reporter);
+                set.add(reportee);
+                reportedMap.put(reporter, set);
+            }
+            else {
+                Set<String> set = new HashSet<>();
+                set.add(reportee);
+                reportedMap.put(reporter, set);
+            }
+
+            Set<String> tempSet = idMap.get(reportee);
+            tempSet.add(reporter);
+            idMap.put(reportee, tempSet);
+        }
+
+        for(int i = 0; i < id_list.length; i++) {
+            int count = 0;
+            if(reportedMap.containsKey(id_list[i])) {
+                Set<String> checkSet = reportedMap.get(id_list[i]);
+                Iterator<String> iter = checkSet.iterator();
+                while(iter.hasNext()) {
+                    String id = iter.next();
+                    if(idMap.get(id).size() >= k) {
+                        count++;
+                    }
+                }
+            }
+
+            answer[i] = count;
+        }
+
+
+
+        return answer;
+    }
+
+    public int[] failSol(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
         Map<String, Integer> map = new HashMap<>();
         HashSet<String> set = new HashSet<String>();
         //report 중복 제거
